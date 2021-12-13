7f56205be584f79e0610c5650d15eca5c71b3593
wildanfajri1alfarabi <wildanfajri1alfarabi@apps.ipb.ac.id>
2021-12-13 14:16:30 +0700

    Settings and Build the game
    v1.0

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,12 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject levelSelector;
+    public GameObject levelSelector, settingWindow, creditWindow;
 
     public void playGame()
     {
         levelSelector.SetActive(true);
     }
+
+    public void openSettings()
+    {
+        settingWindow.SetActive(true);
+    }
+
+    public void closeSettings()
+    {
+        settingWindow.SetActive(false);
+    }
+
+    public void openCredit()
+    {
+        creditWindow.SetActive(true);
+    }
+
+    public void closeCredit()
+    {
+        creditWindow.SetActive(false);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
 
     public void quitGame()
     {
